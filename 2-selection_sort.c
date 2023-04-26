fe67c4b68707acb97ca8f3365dee2aed745ec4e2
Gilbert Adikankwu <gilbert945@outlook.com>
2023-04-26 22:05:50 +0100

    fix: Add checks for null array or single elemement array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,6 +12,9 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min;
 	int temp;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		/* set initial minimum value */
