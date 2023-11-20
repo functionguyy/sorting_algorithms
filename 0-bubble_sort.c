2a95fd2eaf6a5e10f9536d5782e00a3da76f4591
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-20 13:08:00 +0100

    feat: add bubble sort algo

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+#include <stdbool.h>
+
+/**
+ * bubble_sort - sort an array in ascending order.
+ * @array: Array of integers to be sorted.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap correctly.
+ */
+void bubble-sort(int *array, size_t size)
+{
+	size_t i;
+	int temp;
+	bool swapped;
+
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (swapped != false)
+	{
+		swapped = false;
+		for (i = 1; i < size; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				temp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = temp;
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+	}
+}
